0281328e1557362c70cc2b31cc76a1f32d655fc9
Chansik Yun <chansigi@gmail.com>
2018-10-15 11:44:21 +0900

    .

---
+++ src/02_기본문법.kt
@@ -0,0 +1,34 @@
+import java.util.*
+
+// 1. main 함수를 만드는 방법
+// 2. 함수를 만드는 방법
+//  fun 함수이름(파라미터 변수명: 타입): 반환타입 {}
+//   Java: 전역 함수를 만들 수 없다.
+//     Collections / Arrays / Objects
+//  Kotlin: 전역 함수를 만들 수 있다.
+//     별도의 파일에 전역 함수들로만 구성할 수 있다.
+
+// 3. Kotlin = OOP + FP(함수형 프로그래밍)
+//   순수 함수(pure function)
+//    = f(x, y) => x + y + g++ -> X
+//    : 함수의 입력값이 동일하면 항상 결과는 동일하다.
+//    = f(x, y) => x + y => z
+
+//  Java        Kotlin
+//  void   ->    Unit
+// Object  ->    Any
+
+// 4.   
+
+
+fun main(args: Array<String>) {
+    println(println("hello"))
+}
+
+
+
+
+
+
+
+
---
+++ src/samples/JavaProgram.java
@@ -0,0 +1,7 @@
+package samples;
+
+public class JavaProgram {
+    public static void main(String[] args) {
+        // System.out.println(System.out.println());
+    }
+}
