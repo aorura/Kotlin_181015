416ea50cba163684129d230ce7d74af8b1a0665f
Chansik Yun <chansigi@gmail.com>
2018-10-17 15:15:21 +0900

    .

--- src/19_인라인.kt
+++ src/19_인라인.kt
@@ -7,7 +7,10 @@ import java.util.concurrent.locks.ReentrantLock
 //  : 임계 영역에서 예외가 발생할 경우, 데드락의 위험성이 있다.
 //   => RAII
 
-fun <T> withLock(lock: Lock, action: () -> T): T {
+// Inline
+//  : 람다 파라미터를 가지고 있는 함수에서 작동하는 기능이다.
+//    불필요한 코드를 제거하고 실행하는 메소드 안으로 복사-붙여넣기 하는 기능이다.
+inline fun <T> withLock(lock: Lock, action: () -> T): T {
     lock.lock()
     try {
         return action()
@@ -20,7 +23,23 @@ class IncThread(private val lock: Lock): Thread() {
     companion object {
         var n = 0
     }
+    /*
+    fun run2() {
+        // for (i = 1 ; i <= 1000000 ; ++i)
+        // Kotlin: Range
+        for (e in 1..1000000) {
+            lock.lock()
+            try {
+                ++n
+            } finally {
+                lock.unlock()
+            }
+        }
+    }
+    */
 
+    // withLock에 대한 호출이 발생하면 안된다.
+    // 호출하지 말고, 기계어를 실제로 붙여넣어야 한다. - inline
     override fun run() {
         // for (i = 1 ; i <= 1000000 ; ++i)
         // Kotlin: Range
