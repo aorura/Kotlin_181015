00861d1171a283d48d1d924c161433e7abc77d94
Chansik Yun <chansigi@gmail.com>
2018-10-18 15:18:45 +0900

    .

--- Android/FirstApp/app/src/main/kotlin/xyz/ourguide/firstapp/SignInActivity.kt
+++ Android/FirstApp/app/src/main/kotlin/xyz/ourguide/firstapp/SignInActivity.kt
@@ -3,11 +3,14 @@ package xyz.ourguide.firstapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+// 1. Activity Source 추가
+// 2. AndroidManifest.xml 등록
+// 3. Layout 파일 추가
+
 // AppCompatActivity
-class SignInActivity: AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
     }
 }
