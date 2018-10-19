8c0b7ba3556f14ad2f878f18f57b77ff2c191571
Chansik Yun <chansigi@gmail.com>
2018-10-19 10:13:31 +0900

    .

--- Android/github-android/app/src/main/java/xyz/ourguide/github/SignInActivity.kt
+++ Android/github-android/app/src/main/java/xyz/ourguide/github/SignInActivity.kt
@@ -1,9 +1,11 @@
 package xyz.ourguide.github
 
+import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.customtabs.CustomTabsIntent
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import org.jetbrains.anko.toast
 
@@ -27,6 +29,9 @@ import org.jetbrains.anko.toast
 
 class SignInActivity : AppCompatActivity() {
     companion object {
+        // const val TAG = "SignInActivity"
+        val TAG: String = SignInActivity::class.java.simpleName
+
         const val CLIENT_ID = "b86fc618e4d1e442b8c6"
         const val CLIENT_SECRET = "f784827f1346160399520a39a57cbd5070c0ffe7"
     }
@@ -46,7 +51,7 @@ class SignInActivity : AppCompatActivity() {
                 appendQueryParameter("client_id", CLIENT_ID)
             }.build()
 
-            toast(authUri.toString())
+            // toast(authUri.toString())
 
             // 해당 Uri를 브라우저를 통해서 열기
             val intent = CustomTabsIntent.Builder().build()
@@ -54,10 +59,23 @@ class SignInActivity : AppCompatActivity() {
 
             // 브라우저에서 로그인에 성공하면, hubclient://authorize 로 이동합니다.
             // => AndroidManifest.xml 에 해당 정보를 추가해야 합니다.
+            // => launchMode를 singleTask로 지정해야 합니다.
+        }
+    }
 
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        // toast("OnNewIntent")
+
+        val code = intent?.data?.getQueryParameter("code")
+        if (code == null) {
+            toast("code가 존재하지 않습니다.")
+            return;
         }
 
-
+        // code 를 이용해서, github API 서버로부터 AccessToken을 받아 와야 합니다.
+        Log.i(TAG, code)
     }
 }
 
